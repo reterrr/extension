3bde6948774f2e6f315af5a4bdbb2838b983482b
reterrr <133253145+reterrr@users.noreply.github.com>
2026-09-17 07:15:24 +0200

    Enable workspace capture bridge in Import Review

--- src/sidepanel/main.tsx
+++ src/sidepanel/main.tsx
@@ -1,6 +1,7 @@
 import "../shared/domain/schema.js";
 import "../shared/domain/geographyRuntime";
 import "../shared/domain/core.js";
+import "./importReviewWorkspaceBridge";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
