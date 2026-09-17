c2883aef174e2059e3147c34b41982f29b8a5fe9
reterrr <133253145+reterrr@users.noreply.github.com>
2026-09-17 18:31:48 +0200

    Normalize funding ranges in drafts

--- src/shared/commits/normalize.ts
+++ src/shared/commits/normalize.ts
@@ -1,3 +1,4 @@
+import { normalizeFundingRanges } from "../domain/normalizeFundingRanges";
 import type { DraftCommit } from "../types/commit";
 
 /**
@@ -6,6 +7,7 @@ import type { DraftCommit } from "../types/commit";
  * only Commit to SQLite advances that revision.
  */
 export function normalizeDraftWorkingRevision(draft: DraftCommit): DraftCommit {
+  normalizeFundingRanges(draft.workingState);
   draft.workingState.revision = draft.baseRevision;
   return draft;
 }
